687608d3a228089735e76a8bb6d3e8630b6e5af5
James Massey <jammassey@hotmail.com>
2022-09-24 00:30:45 +0100

    small tweak

--- zorak_bot/core/cogs/optional/auto_persist.py
+++ zorak_bot/core/cogs/optional/auto_persist.py
@@ -4,12 +4,13 @@ from discord.ext import commands, tasks
 
 logger = logging.getLogger(__name__)
 
+
 class PersistCog(commands.Cog):
     def __init__(self, bot):
         self.bit = 0
         self.bot = bot
         self.persist.start()
-        
+
     @commands.command()
     @commands.is_owner()
     async def stop_persist(self, ctx):
@@ -17,7 +18,7 @@ class PersistCog(commands.Cog):
 
     @tasks.loop(seconds=300.0)
     async def persist(self):
-        logger.debug("I will persist") #Potentially not needed
+        logger.debug("I'm creating some background noise!")  # Potentially not needed
         if self.bit == 0:
             self.bit = 1
         else:
@@ -25,7 +26,9 @@ class PersistCog(commands.Cog):
 
     @persist.before_loop
     async def before_persist(self):
+        logger.info("Zorak is getting ready to create some background noise!")
         await self.bot.wait_until_ready()
-        
+
+
 def setup(bot):
     bot.add_cog(PersistCog(bot))
