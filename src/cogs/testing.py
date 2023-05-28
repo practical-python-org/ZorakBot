84420102121e8db09046e21707e8f74c99eef965
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-05-28 19:17:38 +0200

    deployment testing - Ignore me

---
+++ src/cogs/testing.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+
+class Ping(commands.Cog, command_attrs=dict(hidden=True)):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.startswith('!ping'):
+            await message.channel.send(f'Ping: {self.bot.latencey}ms')
+
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
