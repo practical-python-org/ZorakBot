f3fc15b7c6a35b8021f7e65a28c25390d790f5b5
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-02 12:01:14 +0200

    Added ability to add second dropdown in the menu
    This paves the way for multiple dropdowns, and the ability to edit all your personal roles with one single command

--- src/cogs/reaction_roles.py
+++ src/cogs/reaction_roles.py
@@ -1,90 +1,69 @@
-"""
-This cog handles reaction roles.
-Not sure how to handle multiple role-sets yet.
- - New cog?
- - New class?
-"""
 import discord
 from discord.ext import commands
 from ._settings import fun_roles
 
 
-class RoleMenu(discord.ui.View):
+class Skill(discord.ui.Select):
     """
-    This is the logic for the reaction roles.
-    It holds the dropdown in the @discord.ui.select
-    And then the logic inside the select_callback()
+    This is the first box in the command
     """
-    async def on_timeout(self):
-        for button in self.children:
-            button.disabled = True
-        await self.message.edit(view=self)
-
-    @discord.ui.select(
-        placeholder="What is your skill level?",
-        min_values=1,
-        max_values=1,
-        options=[
+    def __init__(self):
+        options = [
             discord.SelectOption(
                 label="Beginner",
+                emoji='🟢',
                 description="Have little to no Python Experience"
             ),
             discord.SelectOption(
                 label="Intermediate",
+                emoji='🟡',
                 description="Can solve issues and diagnose problems"
             ),
             discord.SelectOption(
                 label="Professional",
+                emoji='🔴',
                 description="Using Python in professional life, or just really good."
             )
         ]
-    )
-    async def select_callback(self, select, interaction):
+        super().__init__(placeholder="Select your skill level!", max_values=1, min_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
         """
-        Define some things we use all over the place.
-        """
+                Define some things we use all over the place.
+                """
         member = interaction.user
         members_roles = interaction.user.roles
-        selection = select.values[0].lower()
+        selection = self.values[0].lower()
+        beginner_role = discord.utils.get(member.guild.roles, id=fun_roles["beginner"])
+        intermediate_role = discord.utils.get(member.guild.roles, id=fun_roles["intermediate"])
+        professional_role = discord.utils.get(member.guild.roles, id=fun_roles["professional"])
 
-        beginner_role = discord.utils.get(
-            member.guild.roles, id=fun_roles["beginner"]
-        )
-        intermediate_role = discord.utils.get(
-            member.guild.roles, id=fun_roles["intermediate"]
-        )
-        professional_role = discord.utils.get(
-            member.guild.roles, id=fun_roles["professional"]
-        )
-
+        """
+        Start some logic
+        """
         if selection == 'beginner':
             if str(fun_roles[selection]) in str(members_roles):
-                # If the role exists on the person, simply remove it.
-                await member.remove_roles(
-                    beginner_role
-                )
+                """
+                If the role exists on the person, simply remove it.
+                """
+                await member.remove_roles(beginner_role)
                 await interaction.response.send_message(
                     f" - Removed role: <@&{fun_roles[selection]}>"
                     , ephemeral=True
                 )
 
             elif str(fun_roles[selection]) not in str(members_roles):
-                # If the role is NOT in the users roles, add it, and
-                # remove the other related roles if they exist.
+                """
+                If the role is NOT in the users roles, add it, and 
+                remove the other related roles if they exist. 
+                """
                 removals = ''
-                await member.add_roles(
-                    beginner_role
-                )
+                await member.add_roles(beginner_role)
                 if str(fun_roles['intermediate']) in str(members_roles):
-                    await member.remove_roles(
-                        intermediate_role
-                    )
+                    await member.remove_roles(intermediate_role)
                     removals = f"{removals}\n - Removed: <@&{fun_roles['intermediate']}>"
-
                 if str(fun_roles['professional']) in str(members_roles):
-                    await member.remove_roles(
-                        professional_role
-                    )
+                    await member.remove_roles(professional_role)
                     removals = f"{removals}\n - Removed: <@&{fun_roles['professional']}>"
 
                 await interaction.response.send_message(
@@ -94,31 +73,27 @@ class RoleMenu(discord.ui.View):
 
         elif selection == 'intermediate':
             if str(fun_roles[selection]) in str(members_roles):
-                # If the role exists on the person, simply remove it.
-                await member.remove_roles(
-                    intermediate_role
-                )
+                """
+                If the role exists on the person, simply remove it.
+                """
+                await member.remove_roles(intermediate_role)
                 await interaction.response.send_message(
                     f" - Removed role: <@&{fun_roles[selection]}>"
                     , ephemeral=True
                 )
 
             elif str(fun_roles[selection]) not in str(members_roles):
-                # If the role is NOT in the users roles, add it, and
-                # remove the other related roles if they exist.
+                """
+                If the role is NOT in the users roles, add it, and 
+                remove the other related roles if they exist. 
+                """
                 removals = ''
-                await member.add_roles(
-                    intermediate_role
-                )
+                await member.add_roles(intermediate_role)
                 if str(fun_roles['beginner']) in str(members_roles):
-                    await member.remove_roles(
-                        beginner_role
-                    )
+                    await member.remove_roles(beginner_role)
                     removals = f"{removals}\n - Removed: <@&{fun_roles['beginner']}>"
                 if str(fun_roles['professional']) in str(members_roles):
-                    await member.remove_roles(
-                        professional_role
-                    )
+                    await member.remove_roles(professional_role)
                     removals = f"{removals}\n - Removed: <@&{fun_roles['professional']}>"
 
                 await interaction.response.send_message(
@@ -127,32 +102,28 @@ class RoleMenu(discord.ui.View):
                 )
         elif selection == 'professional':
             if str(fun_roles[selection]) in str(members_roles):
-                # If the role exists on the person, simply remove it.
-                await member.remove_roles(
-                    professional_role
-                )
+                """
+                If the role exists on the person, simply remove it.
+                """
+                await member.remove_roles(professional_role)
                 await interaction.response.send_message(
                     f" - Removed role: <@&{fun_roles[selection]}>"
                     , ephemeral=True
                 )
 
             elif str(fun_roles[selection]) not in str(members_roles):
-                # If the role is NOT in the users roles, add it, and
-                # remove the other related roles if they exist.
+                """
+                If the role is NOT in the users roles, add it, and 
+                remove the other related roles if they exist. 
+                """
                 removals = ''
-                await member.add_roles(
-                    professional_role
-                )
+                await member.add_roles(professional_role)
 
                 if str(fun_roles['beginner']) in str(members_roles):
-                    await member.remove_roles(
-                        beginner_role
-                    )
+                    await member.remove_roles(beginner_role)
                     removals = f"{removals}\n - Removed: <@&{fun_roles['beginner']}>"
                 if str(fun_roles['intermediate']) in str(members_roles):
-                    await member.remove_roles(
-                        intermediate_role
-                    )
+                    await member.remove_roles(intermediate_role)
                     removals = f"{removals}\n - Removed: <@&{fun_roles['intermediate']}>"
 
                 await interaction.response.send_message(
@@ -161,28 +132,45 @@ class RoleMenu(discord.ui.View):
                 )
 
 
-class ReactionRoles(commands.Cog):
+class Location(discord.ui.Select):
+    """
+    This is the Second box in the command
+    """
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="North America", emoji="🦅"),
+            discord.SelectOption(label="Europe", emoji="🇪🇺"),
+            discord.SelectOption(label="Asia", emoji="🐼"),
+            discord.SelectOption(label="Oceana", emoji="🐨"),
+            discord.SelectOption(label="South America", emoji="💃"),
+            discord.SelectOption(label="Africa", emoji="🦒")
+        ]
+        super().__init__(placeholder="Select your continent!", max_values=1, min_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(content=f"Your choice is {self.values[0]}!", ephemeral=True)
+
+
+class SelectView(discord.ui.View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+        self.add_item(Skill())
+        self.add_item(Location())
+
+
+class TestRoles(commands.Cog):
     """
     This is the class that defines the actual slash command.
     It uses the view above to execute actual logic.
     """
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(description="Get new roles, or change the ones you have!")
-    async def roles(self, ctx):
-        """
-        This is the main entrypoint for the cog
-        """
-        await ctx.respond(
-            "Edit Reaction Roles"
-            , view=RoleMenu(timeout=180)
-            , ephemeral=True
-        )
+    async def test_roles(self, ctx):
+        await ctx.respond("Edit Reaction Roles", view=SelectView(), ephemeral=True)
 
 
 def setup(bot):
-    """
-    This comment is literally just to chill the linter out.
-    """
-    bot.add_cog(ReactionRoles(bot))
+    bot.add_cog(TestRoles(bot))
