4b0c2b54ad46666d060bc8ce309fd85ebb258b24
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-26 12:40:24 +0200

    LINTING: admin_startup

--- src/cogs/admin_startup.py
+++ src/cogs/admin_startup.py
@@ -1,22 +1,38 @@
-from discord.ext import commands
-from datetime import datetime
+"""
+Called upon startup of our bot.
+Just logs some info. Nothing really.
+"""
 import logging
+from datetime import datetime
+from discord.ext import commands
+
 
 logger = logging.getLogger(__name__)
 
 
-class onStartup(commands.Cog, command_attrs=dict(hidden=True)):
+class OnStartup(commands.Cog):
+    """
+    onStartup is called when the bot reaches a stable state.
+    """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
+        """
+        Here's the juice. It's literally logging.
+        """
         logger.info(
-            f"Successfully logged in as {self.bot.user}/ ID: {self.bot.user.id}"
+            "Successfully logged in as {%s}/ ID: {%s}"
+            , self.bot.user
+            , self.bot.user.id
         )
-        logger.info(f"Started at: {datetime.now()}")
+        logger.info("Started at: {%s}", datetime.now())
         logger.info("Greetings, puny earth-creature.")
 
 
 def setup(bot):
-    bot.add_cog(onStartup(bot))
+    """
+    required.
+    """
+    bot.add_cog(OnStartup(bot))
