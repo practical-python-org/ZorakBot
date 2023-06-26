4a7a4ebf77dceeae7ae9befd20fe736d9e03f1e2
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-26 14:27:25 +0200

    LINTING: rss

--- src/cogs/rss.py
+++ src/cogs/rss.py
@@ -1,3 +1,6 @@
+"""
+Grabs info from an RSS feed, and posts it in the server.
+"""
 from asyncio import sleep
 import discord
 import feedparser
@@ -7,52 +10,61 @@ from ._settings import normal_channel, rss_feed
 
 
 class RSS(commands.Cog):
+    """
+    The main class for the RSS handler.
+    """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def rss_on_ready(self):
-        def get_IDs():
+        """
+        This gets called to check if new stories are ready.
+        """
+        def get_ids():
             """Grabs the most recent article ID from each URL in
             the TOML, so that we can check what we have already sent"""
-            most_recent_IDs = []
+            most_recent_ids = []
 
             for news_source in rss_feed.keys():
                 news_feed = feedparser.parse(rss_feed[news_source])
 
                 id_code = news_feed["entries"][0]["id"]
-                most_recent_IDs.append(id_code)
+                most_recent_ids.append(id_code)
 
-            return most_recent_IDs
+            return most_recent_ids
 
-        def send_news(ID_to_send):
+        def send_news(id_to_send):
             """Uses the RSS ID's we grab from get_IDs that are NOT already in the DB.
             Sends out the news story if it is not in our DB.
             Each news story is parsed 'slightly' different, which is annoying."""
 
-            def send_python_software_foundation(ID_tag):
-                NewsFeed = feedparser.parse(rss_feed["Python_Software_foundation"])
-                id_code = NewsFeed["entries"][0]["id"]
-                if ID_tag == id_code:
+            def send_python_software_foundation(id_tag):
+                newsfeed = feedparser.parse(rss_feed["Python_Software_foundation"])
+                id_code = newsfeed["entries"][0]["id"]
+                if id_tag == id_code:
                     # if the ID code in the article matches the ID we grabbed in get_IDs(), send.
                     html_reader = html2text.HTML2Text()
                     html_reader.ignore_links = False
 
                     embed = discord.Embed(
-                        title=NewsFeed["entries"][0]["title"],
-                        description=f"By: {NewsFeed['entries'][0]['authors'][0]['name']} at the Python Software Foundation!",
+                        title=newsfeed["entries"][0]["title"],
+                        description=f"By: {newsfeed['entries'][0]['authors'][0]['name']}"
+                                    f" at the Python Software Foundation!",
                         color=discord.Color.blue(),
                     )
 
                     embed.add_field(
                         name="Preview: ",
-                        value=f"{html_reader.handle(NewsFeed['entries'][0]['summary'])[0:1015]} ...",
+                        value=f"{html_reader.handle(newsfeed['entries'][0]['summary'])[0:1015]}"
+                              f" ...",
                         inline=False,
                     )
 
                     embed.add_field(
                         name="Read more: ",
-                        value=f"[{NewsFeed['entries'][0]['link']}]({NewsFeed['entries'][0]['link']})",
+                        value=f"[{newsfeed['entries'][0]['link']}]"
+                              f"({newsfeed['entries'][0]['link']})",
                         inline=False,
                     )
                     embed.set_thumbnail(
@@ -60,14 +72,14 @@ class RSS(commands.Cog):
                     )
 
                     return embed
-                else:
-                    return
+                return None
 
-            def send_jetbrains(ID_tag):
+
+            def send_jetbrains(id_tag):
                 jetbrains = rss_feed["jetbrains"]
-                NewsFeed = feedparser.parse(jetbrains)
-                id_code = NewsFeed["entries"][0]["id"]
-                if ID_tag == id_code:
+                newsfeed = feedparser.parse(jetbrains)
+                id_code = newsfeed["entries"][0]["id"]
+                if id_tag == id_code:
                     # if the ID code in the article matches the ID we grabbed in get_IDs(), send.
 
                     html_reader = html2text.HTML2Text()
@@ -76,44 +88,43 @@ class RSS(commands.Cog):
                     # html = html_reader.handle(NewsFeed['entries'][0]['content'][0]['value'])
 
                     embed = discord.Embed(
-                        title=f"**{NewsFeed['entries'][0]['title']}**",
-                        description=f"By: **{NewsFeed['entries'][0]['author']}** at Jetbrains!",
+                        title=f"**{newsfeed['entries'][0]['title']}**",
+                        description=f"By: **{newsfeed['entries'][0]['author']}** at Jetbrains!",
                         color=discord.Color.blue(),
                     )
 
-                    summary_text = f"{NewsFeed['entries'][0]['summary'][0:1015]} ..."
+                    summary_text = f"{newsfeed['entries'][0]['summary'][0:1015]} ..."
                     embed.add_field(
                         name="**Preview: **", value=summary_text, inline=False
                     )
                     embed.add_field(
                         name="**Read more: **",
-                        value=f"[{NewsFeed['entries'][0]['link']}]({NewsFeed['entries'][0]['link']})",
+                        value=f"[{newsfeed['entries'][0]['link']}]"
+                              f"({newsfeed['entries'][0]['link']})",
                         inline=False,
                     )
-                    embed.set_thumbnail(url=NewsFeed["entries"][0]["featuredimage"])
+                    embed.set_thumbnail(url=newsfeed["entries"][0]["featuredimage"])
                     return embed
-                else:
-                    return
+                return None
 
-            embed = send_python_software_foundation(ID_to_send)
-            embed2 = send_jetbrains(ID_to_send)
+            embed = send_python_software_foundation(id_to_send)
+            embed2 = send_jetbrains(id_to_send)
             if embed is not None:
                 return embed
-            else:
-                return embed2
+            return embed2
 
         while True:
-            sent_ID_list = []
+            sent_id_list = []
 
             all_ids = self.bot.db_client.get_all_stories()
-            for ID in all_ids:
-                sent_ID_list.append(ID)
+            for id_feed in all_ids:
+                sent_id_list.append(id_feed)
 
-            print(sent_ID_list)
-            story_queue = get_IDs()
+            print(sent_id_list)
+            story_queue = get_ids()
 
             for entry in story_queue:
-                if entry not in sent_ID_list:
+                if entry not in sent_id_list:
                     news_channel = await self.bot.fetch_channel(
                         normal_channel["news_channel"]
                     )
@@ -126,4 +137,5 @@ class RSS(commands.Cog):
 
 
 def setup(bot):
+    """required"""
     bot.add_cog(RSS(bot))
