b1f044bbf739622f52aad1f1c4dcc83a0cc17938
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-06-27 17:43:21 +0200

    Added /mute_member

--- src/cogs/admin/admin_mute.py
+++ src/cogs/admin/admin_mute.py
@@ -2,6 +2,7 @@
 Admin command for kicking a user.
 """
 import logging
+from datetime import datetime, timedelta
 import discord
 from discord.ext import commands
 from utilities.cog_helpers._embeds import embed_cant_do_that  # pylint: disable=E0401
@@ -10,17 +11,17 @@ from utilities.cog_helpers._embeds import embed_cant_do_that  # pylint: disable=
 logger = logging.getLogger(__name__)
 
 
-class AdminBan(commands.Cog):
+class AdminMute(commands.Cog):
     """
     Command to ban a user. Takes in a name, and a reason.
     """
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(description="Ban a user.")
-    @commands.has_permissions(ban_members=True)
+    @commands.slash_command(description="Mute a user.")
+    @commands.has_permissions(moderate_members=True)
     @commands.has_role("Staff")
-    async def ban_member(self, ctx, target: discord.Member, reason):
+    async def mute_member(self, ctx, target: discord.Member, time, reason):
         """
         Take in a user mention, and a string reason.
         """
@@ -29,30 +30,30 @@ class AdminBan(commands.Cog):
             if not target.guild_permissions.administrator:
 
                 # Message the user, informing them of their fate
-                await target.send(f'## You were banned by {ctx.author.name}.\n'
-                                  f'**Reason:** {reason}\n'
-                                  '\nIf you wish to appeal this ban,'
-                                  ' contact PracticalPythonStaff@gmail.com')
+                await target.send(f'## You were muted by {ctx.author.name}.\n'
+                                  f'**Time:** {time} minutes')
                 # Then we do the ban
-                await target.ban(reason=f"{ctx.author.name} - {reason}")
-                logger.info("{%s} banned {%s}. Reason: {%s}"
+                time_in_mins = datetime.utcnow() + timedelta(minutes=int(time))
+                await target.timeout(time_in_mins, reason=None)
+                logger.info("{%s} muted {%s} for {%s} minutes. Reason: {%s}"
                             , ctx.author.name
                             , target.name
+                            , time
                             , reason)
                 # Then we publicly announce what happened.
                 await ctx.respond(
                     embed=embed_cant_do_that(
-                        f"**{ctx.author.name}** banned **{target.name}**"
+                        f"**{ctx.author.name}** muted **{target.name}** for {time} minutes"
                         f"\n**Reason:** {reason}")
                 )
 
             else:
                 await ctx.respond(
-                    embed=embed_cant_do_that("You can't ban an Admin.")
+                    embed=embed_cant_do_that("You can't mute an Admin.")
                     , ephemeral=True)
         else:
             await ctx.respond(
-                embed=embed_cant_do_that("You cant ban a bot.")
+                embed=embed_cant_do_that("You cant mute a bot.")
                 , ephemeral=True)
 
 
@@ -60,4 +61,4 @@ def setup(bot):
     """
     required.
     """
-    bot.add_cog(AdminBan(bot))
+    bot.add_cog(AdminMute(bot))
