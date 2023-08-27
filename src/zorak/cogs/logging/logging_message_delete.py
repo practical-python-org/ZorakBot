012c7a553384b0d65827a4284f24aae0fc3a27e7
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-08-27 19:31:38 +0200

    small junk removal

--- src/zorak/cogs/logging/logging_message_delete.py
+++ src/zorak/cogs/logging/logging_message_delete.py
@@ -21,18 +21,16 @@ class LoggingMessageDelete(commands.Cog):
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         """
-        then we grab the guild, and from there read the last entry in the audit log.
-        Discord is stupid right now, and message deletes in the audit log suck,
-        so we cant use this now.
-        https://discordpy.readthedocs.io/en/stable/api.html
-        ?highlight=audit%20log#discord.AuditLogAction.message_delete
+        If a mod deletes, take the audit log event. If a user deletes, handle it normally.
         """
         current_guild = self.bot.get_guild(self.bot.server_settings.server_info['id'])
         audit_log = [entry async for entry in current_guild.audit_logs(limit=1)][0]
         logs_channel = await self.bot.fetch_channel(self.bot.server_settings.log_channel["chat_log"])
+
         # If the audit log is triggered, it means someone OTHER than the author deleted the message.
+        # https://discordpy.readthedocs.io/en/stable/api.html
+        # ?highlight=audit%20log#discord.AuditLogAction.message_delete
         if str(audit_log.action) == 'AuditLogAction.message_delete':
-            member = current_guild.get_member(audit_log.user.id)
             embed = embed_message_delete(audit_log.user, message)
             await logs_channel.send(embed=embed)
 
