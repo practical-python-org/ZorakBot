d736c29f2e96f74ddb4a0ed6e6199e0bb4f846ca
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-09-07 16:17:54 +0200

    Some restructuring / efficeney changes
    Added things to the class / made some computational changes.
    
    Co-Authored-By: cake <79643779+cakescode@users.noreply.github.com>

--- src/zorak/cogs/general/general_trans.py
+++ src/zorak/cogs/general/general_trans.py
@@ -1,74 +1,81 @@
+
 import discord.ext
 import googletrans
 from discord.ext import commands
 from googletrans import Translator
 
+
+def is_multi_lang(lang):
+    if isinstance(lang, list):
+        return True
+    else:
+        return False
+
+
 class googletrans_func(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
+        self.gl = googletrans.LANGUAGES
+        self.translator = Translator()
+        self.threshhold = 0.85
 
-	@commands.Cog.listener()
-	async def on_message(self, message):
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
 
-		# default values
-		THRESHOLD = 0.85 # confidence threshold
-		multi_lang = False # check if detect() returns list
+        detected = self.translator.detect(message.content)
+        multi_lang = is_multi_lang(detected.lang)
 
-		try:
-			if message.author.bot: return
-			translator = Translator()
-			detected = translator.detect(message.content)
-			lang, confidence = detected.lang, detected.confidence
-			# if detection picks up 2 potential languages
-			if isinstance(lang, list):
-				multi_lang = True
-				confidence = confidence[0] # extract value from confidence list
-				lang, lang_2 = lang[0], lang[1] # extract both langs
-			# if english or less than 80% sure its not english & not > 1 language
-			if lang == 'en' or confidence < THRESHOLD and not multi_lang: return
-			if lang == 'zh-CN': lang = 'zh-cn' # fix bc the dict keys dont match
-			# translate message
-			translation = translator.translate(message.content, dest='en')
-			gl = googletrans.LANGUAGES # googletrans dictionary of languages
-			if multi_lang: detected_language = f'{gl[lang]}, {gl[lang_2]}'
-			else: detected_language = f'{gl[lang]}'
-			# send results of translation as embed
-			embed = discord.Embed(
-				title=f'{message.content}:',
-				description=f'{translation.text}')
-			embed.set_footer(text=f'''
-translated from {detected_language}
-confidence: {confidence * 100}%''')
-			await message.channel.send(embed=embed)
-		# if error, send error message to channel that caused it
-		except Exception as e:
-			print(e) # TODO: add pipe to zorak error cog (idk what it is lol sry)
+        if 'en' in detected.lang and detected.confidence > self.threshhold:
+            # If the message is english, just stop
+            return
 
-	@commands.slash_command()
-	async def translate(self, ctx, langauge, message):
-		try:
-			gl = googletrans.LANGUAGES
-			translator = Translator()
-			translated = translator.translate(message, dest=langauge)
-			embed = discord.Embed(
-				title=f'{message}:',
-				description=translated.text
-			)
-			embed.set_footer(text=f'translated from {gl[langauge]}')
-			await ctx.respond(embed=embed)
-		except ValueError as v:
-			languages = ''
-			for key, value in gl.items():
-				languages += f'{key}: {value}\n'
-			embed = discord.Embed(
-				title='Valid Language Codes:',
-				description=languages
-			)
-			embed.set_footer(text='''Please use the code on the left
-to select the language on the right''')
-			await ctx.send(embed=embed)
-		except Exception as e:
-			print(e) # TODO: add pipe to zorak error cog (idk what it is lol sry)
+        if multi_lang:
+            lang = detected.lang[0].lower()
+            lang_2 = detected.lang[1].lower()
+            confidence = detected.confidence[0]  # extract value from confidence list
+            detected_language = f'{self.gl[lang]}, {self.gl[lang_2]}'
+
+        else:
+            lang = detected.lang.lower()
+            confidence = detected.confidence
+            detected_language = f'{self.gl[lang]}'
+
+        # translate message
+        translation = self.translator.translate(message.content, dest='en')
+
+        # send results of translation as embed
+        embed = discord.Embed(
+            title=f'{message.content}:',
+            description=f'{translation.text}')
+
+        embed.set_footer(text=f"translated from {detected_language}\nconfidence: {confidence * 100}%")
+        await message.channel.send(embed=embed)
+
+    @commands.slash_command()
+    async def translate(self, ctx, input_language, text):
+        try:
+            translated = self.translator.translate(text, dest=input_language)
+            embed = discord.Embed(
+                title=f'{text}:',
+                description=translated.text
+            )
+            embed.set_footer(text=f'translated from {self.gl[input_language]}')
+            await ctx.respond(embed=embed)
+
+        except ValueError as v:
+            languages = ''
+            for key, value in self.gl.items():
+                languages += f'{key}: {value}\n'
+            embed = discord.Embed(
+                title='Valid Language Codes:',
+                description=languages
+            )
+            embed.set_footer(text='''Please use the code on the left
+    to select the language on the right''')
+            await ctx.respond(embed=embed)
+
 
 def setup(bot):
-	bot.add_cog(googletrans_func(bot))
+    bot.add_cog(googletrans_func(bot))
