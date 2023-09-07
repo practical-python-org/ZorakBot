39a99c7afdeb562464b4094a2241343cf0fea700
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-09-07 16:30:41 +0200

    Added logging, and docstrings for the linter

--- src/zorak/cogs/general/general_trans.py
+++ src/zorak/cogs/general/general_trans.py
@@ -1,3 +1,8 @@
+"""
+A cog that adds translations to Zorak using the googletrans library
+"""
+
+import logging
 
 import discord.ext
 import googletrans
@@ -5,7 +10,11 @@ from discord.ext import commands
 from googletrans import Translator
 
 
+logger = logging.getLogger(__name__)
+
+
 def is_multi_lang(lang):
+    """Tests if input is a list."""
     if isinstance(lang, list):
         return True
     else:
@@ -13,24 +22,32 @@ def is_multi_lang(lang):
 
 
 class googletrans_func(commands.Cog):
+    """Main class for googletrans"""
     def __init__(self, bot):
+        """
+        Here we innit the bot, the translator object, the language list,
+        and our acceptable threshold
+        """
         self.bot = bot
         self.gl = googletrans.LANGUAGES
         self.translator = Translator()
-        self.threshhold = 0.85
+        self.threshold = 0.85
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        """
+        Scans every message for non-english content.
+        """
         if message.author.bot:
             return
 
         detected = self.translator.detect(message.content)
         multi_lang = is_multi_lang(detected.lang)
 
-        if 'en' in detected.lang and detected.confidence > self.threshhold:
+        if 'en' in detected.lang and detected.confidence > self.threshold:
             # If the message is english, just stop
             return
-
+        logger.info("A message by %s was translated.", message.author.name)
         if multi_lang:
             lang = detected.lang[0].lower()
             lang_2 = detected.lang[1].lower()
@@ -55,6 +72,10 @@ class googletrans_func(commands.Cog):
 
     @commands.slash_command()
     async def translate(self, ctx, input_language, text):
+        """
+        A command to specify a translation.
+        """
+        logger.info("%s used the %s command.", ctx.author.name, ctx.command)
         try:
             translated = self.translator.translate(text, dest=input_language)
             embed = discord.Embed(
@@ -78,4 +99,5 @@ class googletrans_func(commands.Cog):
 
 
 def setup(bot):
+    """Docstrings4lyfe"""
     bot.add_cog(googletrans_func(bot))
