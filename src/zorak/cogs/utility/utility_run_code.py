18a53af2372391a1d20a0eb80e7ac4cf64d675ce
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-10-03 15:00:01 +0900

    Update utility_run_code.py
    Fixes after debugging with Zorak in the dev server.
    
    It seemed str.translate(str.maketrans()) was not going to work with strange iOS quotes so I replaced it with a loop of str.replace()s and now it works.  Could potentially behave strangely with quotes from other operating systems, but iPhone is fine.
    
    Changed some formatting for some of the error embeds to look nicer.

--- src/zorak/cogs/utility/utility_run_code.py
+++ src/zorak/cogs/utility/utility_run_code.py
@@ -41,44 +41,47 @@ class UtilityRunCode(commands.Cog):
                     , ctx.command)
 
         # Adjust iOS quotation marks “”‘’ to avoid SyntaxError: invalid character 
-        codeblock.translate(codeblock.maketrans("“”‘’", "\"\"''"))
+        for i, c in enumerate("“‘”’"):
+            codeblock = codeblock.replace(c, "\"'"[i%2])
 
-        piston = PistonAPI()
-
-        if codeblock.startswith("```py") and codeblock.endswith("```"):
+        if codeblock.startswith("```py") and "```" in codeblock[3:]:
             
             # Split input args from codeblock
             _, codeblock, args = codeblock.split("```")
-            args = args.strip().split("\n")
+            args = [arg for arg in args.split("\n") if arg]
 
             # Remove py/python language indicator from codeblock
             codeblock = codeblock.removeprefix("py").removeprefix("thon").strip()
 
-            # Check if number of input() functions matches args from user.
-            if len(args) != codeblock.count("input("):
-                value = 'Please put your input values in order on separate lines after the codeblock:'\
-                        '\n\n\`\`\`py \nx = input("What is your first input: ")\ny = input("What is '\
-                        'your second input: ")\nprint(x)\nprint(y)\n\`\`\`\nmy_first_input\nmy_second_input'
-                embed = self.get_embed("Formatting error", value, True)
-                await ctx.channel.send(embed=embed)
-                return
-            # Else, replace all input()s with values
-            else:
-                for i in re.findall(r"input\(.*\)\n", codeblock):
-                    codeblock = codeblock.replace(i, args.pop(0) + "\n")
+            # Check for input() args or calls to input() function
+            input_count = codeblock.count("input(")
+            if args or input_count:
+                # Check if number of input() functions matches args from user.
+                if len(args) != input_count:
+                    value = 'Please put your input values in order on separate lines after the codeblock:'\
+                            '\n\n\`\`\`py \nx = input("What is your first input: ")\ny = input("What is '\
+                            'your second input: ")\nprint(x)\nprint(y)\n\`\`\`\nmy_first_input\nmy_second_input'
+                    embed = self.get_embed("Formatting error", value, True)
+                    await ctx.channel.send(embed=embed)
+                    return
+                # Else, replace all input()s with values
+                else:
+                    for i in re.findall(r"input\(.*\)\n", codeblock):
+                        codeblock = codeblock.replace(i, f'"""{args.pop(0)}"""\n')
 
+            piston = PistonAPI()
             runcode = piston.execute(language="py", version="3.10.0", code=codeblock)
             embed = self.get_embed("Output:", runcode)
             await ctx.channel.send(embed=embed)
 
         elif codeblock.startswith("'''") and codeblock.endswith("'''"):
-            value = "Did you mean to use a \` instead of\na '?\n\`\`\`py Your code here \`\`\`"
+            value = "Did you mean to use a \` instead of a ' ?\n\`\`\`py Your code here \`\`\`"
             embed = self.get_embed("Formatting error", value, True)
             await ctx.channel.send(embed=embed)
 
         else:
-            value = 'Please place your code inside a code block. (between "\`\`\`py" '\
-                    'and "\`\`\`")\n\n\`\`\`py \nx = "like this"\nprint(x) \n\`\`\`'
+            value = 'Please place your code inside a code block. (between \`\`\`py '\
+                    'and \`\`\`)\n\n\`\`\`py \nx = "like this"\nprint(x) \n\`\`\`'
             embed = self.get_embed("Formatting error", value, True)
             await ctx.channel.send(embed=embed)
 
