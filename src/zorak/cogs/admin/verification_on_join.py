470b0b2d257c614fef5f019db40c1ff6257d16ad
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-11-12 11:43:18 +0100

    Refactored current verification system

--- src/zorak/cogs/admin/verification_on_join.py
+++ src/zorak/cogs/admin/verification_on_join.py
@@ -18,52 +18,67 @@ class LoggingVerification(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.log_channel = await self.bot.fetch_channel(self.bot.server_settings.log_channel["verification_log"])
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member):
-        """On join, do this stuff."""
+    # OLD LOGIC
+
+    async def add_role_and_log(self, member, logging_channel):
         # Add verification role
         await member.add_roles(member.guild.get_role(self.bot.server_settings.unverified_role["needs_approval"]))
 
         # Log unverified join
-        logs_channel = await self.bot.fetch_channel(
-            self.bot.server_settings.log_channel["verification_log"])  # ADMIN user log
-        await logs_channel.send(f"<@{member.id}> joined, but has not verified.")
+        await logging_channel.send(f"<@{member.id}> joined, but has not verified.")
 
-        # Send Welcome
-        guild = member.guild
+    async def send_welcome_message(self, guild, member):
         welcome_message = f"""
-Hi there, {member.mention}
-I'm Zorak, the moderatior of {guild.name}.
+            Hi there, {member.mention}
+            I'm Zorak, the moderator of {guild.name}.
 
-We are very happy that you have decided to join us.
-Before you are allowed to chat, you need to verify that you aren't a bot.
-Dont worry, it's easy. Just go to 
-{self.bot.get_channel(self.bot.server_settings.mod_channel['verification_channel']).mention}
-and click the green button.
+            We are very happy that you have decided to join us.
+            Before you are allowed to chat, you need to verify that you aren't a bot.
+            Dont worry, it's easy. Just go to 
+            {self.bot.get_channel(self.bot.server_settings.mod_channel['verification_channel']).mention}
+            and click the green button.
 
-After you do, all of {guild.name} is availibe to you. Have a great time :-)
-"""
+            After you do, all of {guild.name} is available to you. Have a great time :-)
+            """
         # Send Welcome Message
         try:
             await member.send(welcome_message)
         except discord.errors.Forbidden as catch_dat_forbidden:
             logger.debug(f'{member.name} cannot be sent a DM.')
 
-        time_unverified_kick = 3600  # 1 hour
-        await sleep(time_unverified_kick)
+    async def kick_if_unverified(self, member, time_to_kick, logging_channel):
+
+        await sleep(time_to_kick)
 
         # Start verification timer
         if "Needs Approval" in [role.name for role in member.roles]:
             # Kick timer, in seconds.
-            time_unverified_kick = 3600  # 1 hour
-            await sleep(time_unverified_kick)
+
+            await sleep(time_to_kick)
 
             if "Needs Approval" in [role.name for role in member.roles]:
                 # Log the kick
-                await logs_channel.send(
-                    f"{member.mention} did not verify, auto-removed." f" ({(time_unverified_kick / 3600)} hour/s)")
+                await logging_channel.send(
+                    f"{member.mention} did not verify, auto-removed." f" ({int((time_to_kick / 3600))} hour/s)")
                 await member.kick(reason="Did not verify.")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        guild = member.guild
+        await self.add_role_and_log(guild, self.logs_channel)
+        await self.send_welcome_message(guild, member)
+        await self.kick_if_unverified(member, 3600, self.logs_channel)
+
+    # NEW LOGIC
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        guild = member.guild
+        await self.add_role_and_log(guild, self.logs_channel)
+        await self.send_welcome_message(guild, member)
+        await self.kick_if_unverified(member, 3600, self.logs_channel)
 
 
 def setup(bot):
