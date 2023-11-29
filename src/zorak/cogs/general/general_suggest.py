6be60e0f2773fcbf4718bb8c86f19e9c238ebc72
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-11-29 22:36:48 +0100

    not sure why that worked, needs more testing

--- src/zorak/cogs/general/general_suggest.py
+++ src/zorak/cogs/general/general_suggest.py
@@ -6,10 +6,7 @@ import logging
 import discord
 from discord.ext import commands
 
-from zorak.utilities.cog_helpers._embeds import (
-    embed_suggestion,
-    embed_suggestion_error# pylint: disable=E0401
-)
+from zorak.utilities.cog_helpers._embeds import embed_suggestions, embed_suggestion_error# pylint: disable=E0401
 
 logger = logging.getLogger(__name__)
 
@@ -38,9 +35,11 @@ class GeneralSuggest(commands.Cog):
         '''
         suggest_channel = await self.bot.fetch_channel(self.bot.server_settings.normal_channel["suggestions_channel"])
         if ctx.channel_id == suggest_channel.id:
-            msg = await ctx.respond(embed = embed_suggestion(ctx.author, question))
-            await msg.add_reaction("👍")
-            await msg.add_reaction("👎")
+            msg = await ctx.respond(embed = embed_suggestions(ctx.author, question))
+            up = "👍"
+            down = "👎"
+            await msg.add_reaction(emoji=up)
+            await msg.add_reaction(emoji=down)
         else:
             await ctx.respond(embed=embed_suggestion_error(suggest_channel))
 
