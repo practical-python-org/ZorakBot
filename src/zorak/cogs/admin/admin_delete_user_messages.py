97ac63264c4d1b9f86406dcfe9e92e2454e9f55f
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2024-04-05 09:04:15 +0200

    checking for a staff role is unnecessary when we check for manage_messages, and makes this cog multi-guild acceptable

--- src/zorak/cogs/admin/admin_delete_user_messages.py
+++ src/zorak/cogs/admin/admin_delete_user_messages.py
@@ -1,6 +1,5 @@
 """
 Admin command for deleting a users historical messages
-TODO: Finish this cog. It is currently named with a _ prefix so that it is not loaded.
 """
 import logging
 import time
@@ -26,7 +25,6 @@ class AdminDeleteMessages(commands.Cog):
 
     @commands.slash_command(description="Deletes all messages from a user over a timeperiod in minutes.")
     @commands.has_permissions(manage_messages=True)
-    @commands.has_role("Staff")
     async def delete_messages(self, ctx, target: discord.Member, minutes):
         """
         Take in a user, and a timeperiod in minutes.
@@ -35,15 +33,15 @@ class AdminDeleteMessages(commands.Cog):
         counter = 0
         time_ago = datetime.utcnow() - timedelta(minutes=int(minutes))
 
-        for guild in self.bot.guilds:
-            for channel in guild.text_channels:
-                async for message in channel.history(limit=300, after=time_ago):
-                    if message.author.id == target.id:
-                        await message.delete()
-                        time.sleep(0.3)
-                        counter += 1
+        for channel in ctx.guild.text_channels:
+            async for message in channel.history(limit=300, after=time_ago):
+                if message.author.id == target.id:
+                    await message.delete()
+                    time.sleep(0.2)
+                    counter += 1
 
-        await ctx.followup.send(embed=embed_cant_do_that(f"{ctx.author.name} deleted {str(counter)} messages by {target.name}."))
+        await ctx.followup.send(
+            embed=embed_cant_do_that(f"{ctx.author.name} deleted {str(counter)} messages by {target.name}."))
 
 
 def setup(bot):
