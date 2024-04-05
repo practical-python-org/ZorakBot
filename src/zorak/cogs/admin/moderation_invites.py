aa29be976d03f4af63ce6d58ae7838f8c982e141
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2024-04-05 10:16:09 +0200

    fix small bug in moderation_invites.py with the message types being passed into Guild Settings

--- src/zorak/cogs/admin/moderation_invites.py
+++ src/zorak/cogs/admin/moderation_invites.py
@@ -22,81 +22,85 @@ class ModerationInvites(commands.Cog):
         """
         Scans every message with the regex below.
         """
-        txt = message.content
-        current_channel = message.channel
-        settings = GuildSettings(self.bot.settings.server, message.guild)
+        if str(message.type) == "MessageType.application_command":
+            return
+        else:
+            txt = message.content
+            current_channel = message.channel
+            author = message.author
+            settings = GuildSettings(self.bot.settings.server, message.guild)
 
-        def is_invite(arg_message):
-            """
-            - invitation types
-            -> Official covers official invites "discord.gg/s7s8df9a"
-            -> unofficial urls that start with d and end with letter numbers "dxxxx.gg/23bn2u2"
-            """
-            official = re.search(
-                "(?:https?://)?(?:www\.|ptb\.|canary\.)?(?:discord(?:app)?\.(?:(?:com|gg)"  # pylint: disable=W1401
-                "/invite/[a-z0-9-_]+)|discord\.gg/[a-z0-9-_]+)",  # pylint: disable=W1401
-                arg_message,
-            )
-            unofficial = re.search(
-                "(?:https?://)?(?:www\.)?(?:dsc\.gg|invite\.gg+|discord\.link)"  # pylint: disable=W1401
-                "/[a-z0-9-_]+",  # pylint: disable=W1401
-                arg_message,
-            )
-            if official is not None or unofficial is not None:
-                return True
-            return False
-
-        def log_message(arg_message):
-            """
-            If it finds something, it logs the message
-            """
-            author = arg_message.author
-            embed = discord.Embed(
-                title="<:red_circle:1043616578744357085> Invite removed",
-                description=f"Posted by {arg_message.author}\nIn {'a DM.' if isinstance(arg_message.channel, discord.DMChannel) else arg_message.channel.mention}",
-                color=discord.Color.dark_red(),
-                timestamp=datetime.utcnow(),
-            )
-            embed.set_thumbnail(url=author.avatar)
-            embed.add_field(
-                name="Message: ",
-                value=message.content,  # ToDo: This throws an error when deleting an embed.
-                inline=True,
-            )
-            return embed
-
-        def embed_warning(arg_message):
-            """
-            If it finds something, it sends a warning that the user should quit that shit.
-            """
-            embed = discord.Embed(
-                title="<:x:1055080113336762408> External Invites are not allowed here!",
-                description=f"{arg_message.author}, your message was removed "
-                            f"because it contained an external invite.\nIf this "
-                            f"was a mistake, contact the @staff",
-                color=discord.Color.dark_red(),
-                timestamp=datetime.utcnow(),
-            )
-            return embed
-
-        def check_for_admin_override(arg_message):
-            """
-            Handling for when a MOD user needs to post an invitation
-            """
-            if not message.content.startswith('z.invite '):
+            def is_invite(arg_message):
+                """
+                - invitation types
+                -> Official covers official invites "discord.gg/s7s8df9a"
+                -> unofficial urls that start with d and end with letter numbers "dxxxx.gg/23bn2u2"
+                """
+                official = re.search(
+                    "(?:https?://)?(?:www\.|ptb\.|canary\.)?(?:discord(?:app)?\.(?:(?:com|gg)"  # pylint: disable=W1401
+                    "/invite/[a-z0-9-_]+)|discord\.gg/[a-z0-9-_]+)",  # pylint: disable=W1401
+                    arg_message,
+                )
+                unofficial = re.search(
+                    "(?:https?://)?(?:www\.)?(?:dsc\.gg|invite\.gg+|discord\.link)"  # pylint: disable=W1401
+                    "/[a-z0-9-_]+",  # pylint: disable=W1401
+                    arg_message,
+                )
+                if official is not None or unofficial is not None:
+                    return True
                 return False
 
-            return any(role.id in settings.admin_roles.values() for role in message.author.roles)
+            def log_message(arg_message):
+                """
+                If it finds something, it logs the message
+                """
+                author = arg_message.author
+                embed = discord.Embed(
+                    title="<:red_circle:1043616578744357085> Invite removed",
+                    description=f"Posted by {arg_message.author}\nIn {'a DM.' if isinstance(arg_message.channel, discord.DMChannel) else arg_message.channel.mention}",
+                    color=discord.Color.dark_red(),
+                    timestamp=datetime.utcnow(),
+                )
+                embed.set_thumbnail(url=author.avatar)
+                embed.add_field(
+                    name="Message: ",
+                    value=message.content,  # ToDo: This throws an error when deleting an embed.
+                    inline=True,
+                )
+                return embed
 
-        if is_invite(txt):
-            if isinstance(message.channel, discord.DMChannel):
-                return
+            def embed_warning(arg_message):
+                """
+                If it finds something, it sends a warning that the user should quit that shit.
+                """
+                embed = discord.Embed(
+                    title="<:x:1055080113336762408> External Invites are not allowed here!",
+                    description=f"{arg_message.author}, your message was removed "
+                                f"because it contained an external invite.\nIf this "
+                                f"was a mistake, contact the @staff",
+                    color=discord.Color.dark_red(),
+                    timestamp=datetime.utcnow(),
+                )
+                return embed
 
-            if not check_for_admin_override(txt):
-                logs_channel = await self.bot.fetch_channel(settings.mod_log)
-                await logs_channel.send(embed=log_message(message))
-                await message.delete()
-                await current_channel.send(embed=embed_warning(message))
+            def check_for_admin_override(arg_message):
+                """
+                Handling for when a MOD user needs to post an invitation
+                """
+                if not message.content.startswith('z.invite '):
+                    return False
+
+                return any(role.id in settings.admin_roles.values() for role in message.author.roles)
+
+            if is_invite(txt):
+                if isinstance(message.channel, discord.DMChannel):
+                    return
+
+                if not check_for_admin_override(txt):
+                    logs_channel = await self.bot.fetch_channel(settings.mod_log)
+                    await logs_channel.send(embed=log_message(message))
+                    await message.delete()
+                    await current_channel.send(embed=embed_warning(message))
 
 
 def setup(bot):
