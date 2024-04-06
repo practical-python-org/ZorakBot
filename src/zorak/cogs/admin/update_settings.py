87150a8db3c63ab85cf4dc4099c0f85fd2b178bd
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2024-04-06 15:42:42 +0200

    [MAJOR] - Update command to update settings. Doesn't feel like final product material

---
+++ src/zorak/cogs/admin/update_settings.py
@@ -0,0 +1,37 @@
+"""
+A simple hello command.
+"""
+import logging
+from random import shuffle
+import discord
+from discord.ext import commands
+from time import sleep
+
+logger = logging.getLogger(__name__)
+
+
+
+class Settings(commands.Cog):
+    """
+    This is the class that defines the actual slash command.
+    It uses the view above to execute actual logic.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot  # Passed in from main.py
+
+    @commands.slash_command(description="Edit the bot settings for your guild!")
+    async def update_settings(self, ctx, setting: str, value: int):
+        """The slash command that initiates the fancy menus."""
+        self.bot.db_client.update_guild_settings(ctx.guild, setting, int(value))
+
+        await ctx.respond(f"Updated {setting} in {ctx.guild.name}. New Value: {value}")
+
+
+
+
+def setup(bot):
+    """
+    Required.
+    """
+    bot.add_cog(Settings(bot))
