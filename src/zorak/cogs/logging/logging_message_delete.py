78f80e82276704728721a21f459f1922050d2ee0
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2024-04-21 19:21:23 +0200

    - Dont log staff channels. - If the audit log is triggered, we use ONLY the audit log info, otherwise the message info

--- src/zorak/cogs/logging/logging_message_delete.py
+++ src/zorak/cogs/logging/logging_message_delete.py
@@ -25,15 +25,17 @@ class LoggingMessageDelete(commands.Cog):
         """
         If a mod deletes, take the audit log event. If a user deletes, handle it normally.
         """
+        # Don't record edits in Staff only channels.
+        if message.channel.category_id == 1054761641289449482:  # This is the ID of the "staff area" category.
+            # Yes, that's hardcoded. Suck it.
+            return
+
         current_guild = message.guild
         audit_log = [entry async for entry in current_guild.audit_logs(limit=1)][0]
         logs_channel = await self.bot.fetch_channel(self.bot.server_settings.log_channel["chat_log"])
 
-        logger.info(f" --- A message by {message.author.name} was deleted...")
-
         # If the audit log is triggered, it means someone OTHER than the author deleted the message.
-        # https://discordpy.readthedocs.io/en/stable/api.html
-        # ?highlight=audit%20log#discord.AuditLogAction.message_delete
+        # https://discordpy.readthedocs.io/en/stable/api.html?highlight=audit%20log#discord.AuditLogAction.message_delete
 
         # print(audit_log)
         # print(f"-> {audit_log.action}")
@@ -49,9 +51,9 @@ class LoggingMessageDelete(commands.Cog):
         # print(f"-> {audit_log.target}")
         # print(f"-> {audit_log.user}")
 
-        if str(audit_log.action) == 'AuditLogAction.message_delete' and audit_log.user != audit_log.target:
+        if str(audit_log.action) == 'AuditLogAction.message_delete':
             # Then a moderator deleted a message.
-            embed = embed_message_delete(message.author, message, audit_log.user)
+            embed = embed_message_delete(audit_log.target, message, audit_log.user)
             await logs_channel.send(embed=embed)
 
         else:
